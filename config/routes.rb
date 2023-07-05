e2dc155d0f3fc84defbeb820b4607473c38ca70e
kent0722 <enginer@eamil.com>
2023-07-05 05:55:17 +0000

    userコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   root 'static_pages#top'
+  get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
